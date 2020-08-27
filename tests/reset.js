2b8819c2460e7d9868aad37c135448571188dd0b
dmitsar <dmitrytsarev97@gmail.com>
2020-08-27 15:12:14 +0300

    update number

--- tests/reset.js
+++ tests/reset.js
@@ -19,11 +19,11 @@ describe('Якобс', function () {
     describe('Перезагрузка видео', function () {
         it('Нажатие кнопки', async () => {
             await Page.sleep(1000)
-            await Page.navigate('/#/objects/2714112/tv/view/6311043/REPEATABLE')
+            await Page.navigate('/#/objects/2714112/tv/view/12869090/REPEATABLE')
             await Page.wait('body > div > ui-view > div > div.app-content-wrap.flex-block.flex-wrap.flex-vertical > div.app-workspace.ng-scope > ui-view > div > ui-view > ui-view > div.new-content-wrap.flex-vertical.action-buttons-in-new-content-wrap.ng-scope > div.object-tv-bottom-block.flex > div.object-tv-left-col.flex-wrap.flex-vertical.flex > div.flex-table-tr.no-highlight.no-select.ng-scope.active-force-highlight > div.flex-table-td.fixed-width.object-tv-left-col-power.text-center > div > div > ki-new-tv-power-icon > span > span.new-tv-power-indicator.on');
             await Page.click('body > div > ui-view > div > div.app-content-wrap.flex-block.flex-wrap.flex-vertical > div.app-workspace.ng-scope > ui-view > div > ui-view > ui-view > div.new-content-wrap.flex-vertical.action-buttons-in-new-content-wrap.ng-scope > div.object-tv-bottom-block.flex > div.object-tv-left-col.flex-wrap.flex-vertical.flex > div.flex-table-tr.no-highlight.no-select.ng-scope.active-force-highlight > div.flex-table-td.fixed-width.object-tv-left-col-power.text-center > div > div > ki-new-tv-power-icon > span > span.new-tv-power-indicator.on');
             await Page.sleep(1000)
-            await Page.click('div:nth-child(4) > div.flex-table-td.fixed-width.object-tv-left-col-power.text-center > div > div > ki-new-tv-power-icon > ul > li:nth-child(4) > a > svg');
+            await Page.click('body > div > ui-view > div > div.app-content-wrap.flex-block.flex-wrap.flex-vertical > div.app-workspace.ng-scope > ui-view > div > ui-view > ui-view > div.new-content-wrap.flex-vertical.action-buttons-in-new-content-wrap.ng-scope > div.object-tv-bottom-block.flex > div.object-tv-left-col.flex-wrap.flex-vertical.flex > div.flex-table-tr.no-highlight.no-select.ng-scope.active-force-highlight > div.flex-table-td.fixed-width.object-tv-left-col-power.text-center > div > div > ki-new-tv-power-icon > ul > li:nth-child(4) > a');
             await Page.sleep(1000)
         })
         })
--- tests/screen.js
+++ tests/screen.js
@@ -20,10 +20,11 @@ describe('Якобс', function () {
         element = "";
         it('Просмотр видео', async () => {
             await Page.sleep(1000)
-            await Page.navigate('/#/objects/2714112/tv/view/6311043/REPEATABLE')
+            await Page.navigate('/#/objects/2714112/tv/view/12869090/REPEATABLE')
             await Page.wait('body > div > ui-view > div > div.app-content-wrap.flex-block.flex-wrap.flex-vertical > div.app-workspace.ng-scope > ui-view > div > ui-view > ui-view > div.new-content-wrap.flex-vertical.action-buttons-in-new-content-wrap.ng-scope > div.object-tv-bottom-block.flex > div.object-tv-right-col.flex-block.flex-wrap.flex-vertical.ng-scope > ui-view > div.office-playlists-info-wrap-inner.ng-scope.on-object-tv-screen > div.office-playlists-bottom-info-block.playlist-screen-padding.abs-wrap.float-wrap.on-object-tv-screen > div.templates-grid.float-wrap > div.template-in-grid.abs-wrap.float-wrap.ng-scope > div.template-in-grid-preview-wrap.abs-wrap.float-wrap > img:nth-child(1)');
             await Page.sleep(1000)
             element = await Page.dollar('body > div > ui-view > div > div.app-content-wrap.flex-block.flex-wrap.flex-vertical > div.app-workspace.ng-scope > ui-view > div > ui-view > ui-view > div.new-content-wrap.flex-vertical.action-buttons-in-new-content-wrap.ng-scope > div.object-tv-bottom-block.flex > div.object-tv-right-col.flex-block.flex-wrap.flex-vertical.ng-scope > ui-view > div.office-playlists-info-wrap-inner.ng-scope.on-object-tv-screen > div.office-playlists-bottom-info-block.playlist-screen-padding.abs-wrap.float-wrap.on-object-tv-screen > div.templates-grid.float-wrap > div.template-in-grid.abs-wrap.float-wrap.ng-scope > div.template-in-grid-preview-wrap.abs-wrap.float-wrap > img:nth-child(1)');
+            await Page.sleep(1000)
         })
         it('Сохранение скрина', async () => {
             await Page.sleep(5000)
